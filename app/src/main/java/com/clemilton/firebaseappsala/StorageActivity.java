4938db037765c296729a38550719ba99b96ed0fb
clemilton <clemilton.fucapi@gmail.com>
2021-06-18 21:25:08 -0400

    03 - Upload Imagem Storage

--- app/src/main/java/com/clemilton/firebaseappsala/StorageActivity.java
+++ app/src/main/java/com/clemilton/firebaseappsala/StorageActivity.java
@@ -3,13 +3,17 @@ package com.clemilton.firebaseappsala;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -17,6 +21,7 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 import java.io.ByteArrayOutputStream;
+import java.util.Date;
 
 
 public class StorageActivity extends AppCompatActivity {
@@ -24,6 +29,8 @@ public class StorageActivity extends AppCompatActivity {
     private FirebaseStorage storage = FirebaseStorage.getInstance();
     private Button btnUpload,btnGaleria;
     private ImageView imageView;
+    private Uri imageUri=null;
+    private EditText editNome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,9 +38,20 @@ public class StorageActivity extends AppCompatActivity {
         btnUpload = findViewById(R.id.storage_btn_upload);
         imageView = findViewById(R.id.storage_image_cel);
         btnGaleria = findViewById(R.id.storage_btn_galeria);
+        editNome = findViewById(R.id.storage_edit_nome);
 
         btnUpload.setOnClickListener(v ->{
-            uploadImagemByte();
+            if(editNome.getText().toString().isEmpty()){
+                Toast.makeText(this,"Digite um nome para Imagem",
+                                    Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if(imageUri!=null){
+                uploadImagemUri();
+            }else {
+                uploadImagemByte();
+            }
         });
 
         btnGaleria.setOnClickListener( v -> {
@@ -41,14 +59,48 @@ public class StorageActivity extends AppCompatActivity {
             //intent implicita -> pegar um arquivo do celular
             intent.setAction(Intent.ACTION_GET_CONTENT);
             intent.setType("image/*");
-            startActivityForResult(intent,1);
+            startActivityForResult(intent,112);
         });
 
+    }
+
+    private void uploadImagemUri() {
+        String tipo = getFileExtension(imageUri);
+        //referencia do arquivo no firebase
+        Date d = new Date();
+        String nome = editNome.getText().toString()+"-"+d.getTime();
+        StorageReference imagemRef = storage.getReference()
+                                    .child("imagens/"+nome+"."+tipo);
+
+        imagemRef.putFile(imageUri)
+        .addOnSuccessListener(taskSnapshot -> {
+            Toast.makeText(this,"Upload feito com sucesso",
+                                        Toast.LENGTH_SHORT).show();
+        })
+        .addOnFailureListener(e -> {
+            e.printStackTrace();
+        });
+
+    }
+    //retornar o tipo(.png, .jpg) da imagem
+    private String getFileExtension(Uri imageUri) {
+        ContentResolver cr = getContentResolver();
+        return MimeTypeMap.getSingleton()
+                .getExtensionFromMimeType(cr.getType(imageUri));
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        Log.i("RESULT", "requestCode: "+ requestCode +
+                                ",resultCode: "+ resultCode);
+        if(requestCode==112 && resultCode== Activity.RESULT_OK){
+            //caso o usuario selecionou uma imagem da galeria
+
+            //endereco da imagem selecionada
+            imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+        }
     }
 
     public byte[] convertImage2Byte(ImageView imageView){
